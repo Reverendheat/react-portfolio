d7e4a2fa4e38441ff2cc970d641e042a00c72edf
Brandon Sharp <brandon.sharp@llamasoft.com>
2021-10-18 15:30:24 -0400

    Fix spacing on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
       <Container style={{alignItems:'center',height: "100vh",justifyContent: 'center',display:'flex'}}>
           <div>
           <h1 style={{textAlign:'center'}}>Contact me</h1>
-          <div style={{display:"flex", justifyContent:'center', gap: '15em'}}>
+          <div style={{display:"flex", justifyContent:'center', gap: '5em'}}>
             <a target="_blank" href='https://github.com/reverendheat'><AiFillGithub size={70} color="black" /></a>
             <a target="_blank" href='https://www.linkedin.com/in/brandon-sharp-23a87210/'><AiFillLinkedin size={70} color='#0e76a8' /></a>
           </div>
